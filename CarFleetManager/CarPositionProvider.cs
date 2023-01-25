518ba398186692f2c7d8689eb79506885f9b3c19
Lakasabasz <lakasabasz@gmail.com>
2023-01-25 15:31:37 +0100

    Added map control

---
+++ CarFleetManager/CarPositionProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Fetcher;
+using Mapsui.Layers;
+using Mapsui.Projections;
+using Mapsui.Providers;
+
+namespace CarFleetManager;
+
+public class CarPositionProvider : MemoryProvider, IDynamic
+{
+    private readonly Timer _timer;
+    public CarPositionProvider()
+    {
+        _timer = new Timer(_ => DataHasChanged(), this, 0, 1 * 1000);
+    }
+    public override Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
+    {
+        var features = new List<PointFeature>();
+        var position = SphericalMercator.FromLonLat(19.05885, 49.78364).ToMPoint();
+        features.Add(new PointFeature(position)
+        {
+            ["ID"] = "0"
+        });
+        return Task.FromResult((IEnumerable<IFeature>) features);
+    }
+
+    public void DataHasChanged()
+    {
+        DataChanged?.Invoke(this, new DataChangedEventArgs(null, false, null));
+    }
+
+    public event DataChangedEventHandler? DataChanged;
+}
--- CarFleetManager/MainWindow.xaml.cs
+++ CarFleetManager/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Layers.AnimatedLayers;
+using Mapsui.Projections;
+using Mapsui.Styles;
+using Brush = Mapsui.Styles.Brush;
+using Color = Mapsui.Styles.Color;
 
 namespace CarFleetManager
 {
@@ -23,6 +31,20 @@ namespace CarFleetManager
         public MainWindow()
         {
             InitializeComponent();
+            MapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            MapControl.Map?.Layers.Add(CreatePointLayer());
+            var ath = new MPoint(19.05885, 49.78364);
+            MapControl.Navigator?.NavigateTo(SphericalMercator.FromLonLat(ath.X, ath.Y).ToMPoint(), MapControl.Map.Resolutions[16]);
+        }
+
+        private ILayer CreatePointLayer()
+        {
+            return new AnimatedPointLayer(new CarPositionProvider())
+            {
+                Name = "Car position",
+                Style = new SymbolStyle()
+                    { Fill = new Brush(Color.Red), SymbolScale = 0.5, SymbolType = SymbolType.Ellipse },
+            };
         }
     }
 }
